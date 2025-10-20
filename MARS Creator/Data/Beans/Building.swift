22893533aee56967a7f3ef619e643c8eb81c3ad7
Danil Lugli <danillugli@Danils-MacBook-Pro.local>
2025-10-20 09:26:32 +0200

    fix

--- MARS Creator/Data/Beans/Building.swift
+++ MARS Creator/Data/Beans/Building.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import SwiftUICore
+import SwiftUI
 
 class Building: Encodable, ObservableObject, Hashable {
     private var _id = UUID()
--- MARS Creator/Data/Beans/Connection/ConnectionJSON.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ConnectionJSON.swift
-//  ScanBuild
-//
-//  Created by Danil Lugli on 30/09/24.
-//
-
-import Foundation
